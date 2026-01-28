102faa9132c2df5de718e16e59e8de861014c912
Gaaldra <gabrielalves.quadra@gmail.com>
2026-01-28 11:02:52 -0300

    feat(repository): introduce ICategoryRepository interface
    Add a specialized repository interface for the 'Category' model to
    handle domain-specific data operation.

---
+++ Ecommerce.Domain/Repository/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ecommerce.Domain.Models;
+
+namespace Ecommerce.Domain.Repository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    Task<Category?> GetByNameAsync(string categoryName);
+}
