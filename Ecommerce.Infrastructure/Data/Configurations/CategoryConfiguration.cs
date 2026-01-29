0c1d92f41209cc4b3ada666b93c89378bd0fd1d1
Gaaldra <gabrielalves.quadra@gmail.com>
2026-01-29 11:53:04 -0300

    feat(infrastructure): setup ef core and category repository implementation
    Configure AppDbContext with assembly scanning for Fluent API.
    Implement 'ICategoryRepository' using Entity Framework Core.
    Add 'CategoryConfiguration' for database schema mapping.
    Set up base infrastructure folder structure (Data, Context,
    Repositories).

---
+++ Ecommerce.Infrastructure/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Data.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+        builder.HasIndex(x => x.Name).IsUnique();
+    }
+}
---
+++ Ecommerce.Infrastructure/Data/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Data.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
